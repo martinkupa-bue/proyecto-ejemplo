f03ad21d7b623d771cf61b167d0fe7b7a3791a0e
martin <martin@mail.com>
2024-08-08 20:16:31 -0300

    Expandimos el saludo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Este es nuestro primer proyecto con express')
 })
 
 app.listen(port, () => {
